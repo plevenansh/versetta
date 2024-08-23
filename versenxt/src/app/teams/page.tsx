614cfdafb2f5d1b9d1923e0cf4912b3a24121ef6
Ansh Gupta <radixansh@gmail.com>
2024-08-23 12:44:13 +0530

    trying to deploy

--- versenxt/src/app/teams/page.tsx
+++ versenxt/src/app/teams/page.tsx
@@ -1,10 +1,10 @@
-// import TeamOnboarding from '@/components/team';
+//import TeamOnboarding from '@/components/team';
 
-// export default function TeamOnboardingPage() {
-//   return (
-//     <div className="container mx-auto p-4">
-//       <h1 className="text-2xl font-bold mb-4">Team Onboarding</h1>
-//       <TeamOnboarding />
-//     </div>
-//   );
-// }
+export default function TeamOnboardingPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Team Onboarding</h1>
+      {/* <TeamOnboarding /> */}
+    </div>
+  );
+}

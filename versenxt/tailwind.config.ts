37150d845879ff892da8d4141c073d4745fe68ac
Ansh Gupta <radixansh@gmail.com>
2024-09-02 13:24:55 +0530

    checkpoint+non essential remove+tailwind fixws

--- versenxt/tailwind.config.ts
+++ versenxt/tailwind.config.ts
@@ -11,13 +11,13 @@ const config = {
   
   prefix: "",
   theme: {
-  	container: {
-  		center: 'true',
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
+	container: {
+	  center: true,
+	  padding: '2rem',
+	  screens: {
+		'2xl': '1400px'
+	  }
+	},
   	extend: {
   		colors: {
   			border: 'hsl(var(--border))',
@@ -63,45 +63,45 @@ const config = {
   			sans: ["var(--font-manrope)", ...fontFamily.sans]
   		},
   		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
+			'accordion-down': {
+			  from: {
+				height: '0'
+			  },
+			  to: {
+				height: 'var(--radix-accordion-content-height)'
+			  }
+			},
+			'accordion-up': {
+			  from: {
+				height: 'var(--radix-accordion-content-height)'
+			  },
+			  to: {
+				height: '0'
+			  }
+			},
+			'accordion-down2': {
+			  from: {
+				height: '0'
+			  },
+			  to: {
+				height: 'var(--radix-accordion-content-height)'
+			  }
+			},
+			'accordion-up2': {
+			  from: {
+				height: 'var(--radix-accordion-content-height)'
+			  },
+			  to: {
+				height: '0'
+			  }
+			}
   		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
+		animation: {
+		  'accordion-down': 'accordion-down 0.2s ease-out',
+		  'accordion-up': 'accordion-up 0.2s ease-out',
+		  'accordion-down2': 'accordion-down2 0.2s ease-out',
+		  'accordion-up2': 'accordion-up2 0.2s ease-out'
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],

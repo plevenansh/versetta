debf59be8883f97b1712d8d18b2d45068b0116cd
Ansh Gupta <86645806+plevenansh@users.noreply.github.com>
2024-09-11 15:24:05 +0530

    Update page.tsx

--- versenxt/src/app/contact/page.tsx
+++ versenxt/src/app/contact/page.tsx
@@ -14,7 +14,7 @@ const ContactPage = () => {
           <h2 className="text-2xl font-semibold mb-4">Email</h2>
           <a href="mailto:versatter@gmail.com" className="text-blue-600 hover:underline text-xl">
             versatter@gmail.com
-            Kanpur,India
+          Add:  GX,IIT,Kanpur,India
           </a>
         </div>
       </div>

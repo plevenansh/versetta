a845e38a6b4e2d86f8edaed824d7c8464275a80e
Ansh Gupta <86645806+plevenansh@users.noreply.github.com>
2025-02-13 01:42:45 +0530

    Delete client directory

--- client/api.d.ts
+++
@@ -1,6 +0,0 @@
-// api.d.ts
-import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
-import type { AppRouter } from '../versenxt/src/server'; // Adjust this path as needed
-
-export type RouterInput = inferRouterInputs<AppRouter>;
-export type RouterOutput = inferRouterOutputs<AppRouter>;

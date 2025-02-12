eaeb2be01cfc042291cf9826cf61ddc12a4e6013
Ansh Gupta <86645806+plevenansh@users.noreply.github.com>
2025-02-13 01:42:13 +0530

    Delete backend directory

--- backend/dist/lib/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-console.log("yeah bruh we reached in prisma file");
-const prisma = global.prisma || new client_1.PrismaClient();
-if (process.env.NODE_ENV !== 'production')
-    global.prisma = prisma;
-exports.default = prisma;
